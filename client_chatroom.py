ae5e93f46204b86339bd7a7cbcb5618c463f6095
Urpagin <72459611+Urpagin@users.noreply.github.com>
2021-12-26 09:59:42 +0100

    init

---
+++ client_chatroom.py
@@ -0,0 +1,40 @@
+import threading
+import socket
+
+while True:
+    nickname = input("Choose a nickname: ")
+    if nickname.isalnum():
+        break
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('urpagin.xyz', 45588))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("Error receiving message or sending nickname")
+            client.close()
+            break
+
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        try:
+            client.send(message.encode('utf-8'))
+        except:
+            client.send('Error sending message - illegal character.'.encode('utf-8'))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
